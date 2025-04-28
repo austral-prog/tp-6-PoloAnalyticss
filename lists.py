83cf683b523d1b4ca4aeb1ed8224c60e85f13f42
PoloAnalyticss <hmaggi@mail.austral.edu.ar>
2025-04-28 12:42:12 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,20 +1,56 @@
-# Replace the "ANSWER HERE" with your answer
-
 def remove_elements(list_to_remove_elements):
-    return "ANSWER HERE"  # Remove this line and implement
+
+    if len(list_to_remove_elements) > 5:
+        list_to_remove_elements.pop(5)
+    if len(list_to_remove_elements) > 4:
+        list_to_remove_elements.pop(4)
+    if len(list_to_remove_elements) > 0:
+        list_to_remove_elements.pop(0)
+    return list_to_remove_elements
+
+list_to_remove_elements = ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+print(remove_elements(list_to_remove_elements))
 
 
 def add_elements(list_to_add_elements):
-    return "ANSWER HERE"  # Remove this line and implement
+    list_to_add_elements.insert(0, "Pink")
+    list_to_add_elements.append("Yellow")
+    return list_to_add_elements
 
+list_to_add_elements = ['Red', 'Green', 'White', 'Black']
+print(add_elements(list_to_add_elements))
 
 def is_empty(list_to_check):
-    return "ANSWER HERE"  # Remove this line and implement
+    if len(list_to_check) == 0:
+        return True
+    else:
+        return False
+    
+list_to_check = []
+print(is_empty(list_to_check))
+
 
 
 def check_lists(list_to_compare1, list_to_compare2):
-    return "ANSWER HERE"  # Remove this line and implement
+    if len(list_to_compare1) >= 3 and len(list_to_compare2) >= 3:
+        if list_to_compare1[2] == list_to_compare2[2]:
+            return True
+        if list_to_compare1[2] != list_to_compare2[2]:
+            return False
+    if len(list_to_compare1) < 3 and len(list_to_compare2) < 3:
+        return False
+    if len(list_to_compare1) < 3 and len(list_to_compare2) >= 3:
+        return False
+    if len(list_to_compare1) >= 3 and len(list_to_compare2) < 3:
+        return False
+
+    
+list_to_compare1 = ['Black', 'Pink', 'Green', 'White']
+list_to_compare2 = ['Red', 'Green', 'Yellow', 'Black', 'Pink']
+print(check_lists(list_to_compare1, list_to_compare2))
+
+
 
 
 def list_of_lists(list_of_lists_to_modify):
-    return "ANSWER HERE"  # Remove this line and implement
+    
